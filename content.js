788b920b8a2c25d845885209968bfc672df33af4
tomdreamevil <tomdreamevil@gmail.com>
2017-04-02 20:56:46 +0100

    initial commit

---
+++ content.js
@@ -0,0 +1,12 @@
+var thePets = [
+{
+    "name": "tom",
+    "species": "cat",
+    "favFood": "tuna"
+},
+    {
+        "name": "rover",
+        "species": "dog",
+        "favFood": "sweet treats"
+    }
+    ]
